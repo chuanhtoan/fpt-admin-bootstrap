929f7b523a48480859a7d465488f8b17d69986ec
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-19 17:03:35 +0700

    hiding main-nav

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 function headerHam(x) {
 	if (x.classList.contains('change')) {
 		document.querySelector('.ham-container').classList.remove('change')
-		// document.querySelector('.accounts__list').classList.add("hidden")
+		document.querySelector('.main-nav').classList.remove('hidden')
+		document.querySelector('.main-header').classList.remove('ms-0')
+		document.querySelector('.main-header').classList.remove('w-100')
+		document.querySelector('.main-content').classList.remove('ms-0')
+		document.querySelector('.main-content').classList.remove('w-100')
 	} else {
 		document.querySelector('.ham-container').classList.add('change')
-		// document.querySelector('.accounts__list').classList.remove("hidden")
+		document.querySelector('.main-nav').classList.add('hidden')
+		document.querySelector('.main-header').classList.add('ms-0')
+		document.querySelector('.main-header').classList.add('w-100')
+		document.querySelector('.main-content').classList.add('ms-0')
+		document.querySelector('.main-content').classList.add('w-100')
 	}
 }
